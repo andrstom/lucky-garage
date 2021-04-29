b84b0dfc853a738ef239c7da3934d59ffd0161e2
andrstom <andrsak85@gmail.com>
2021-04-29 19:45:07 +0200

    navbar and header styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import LocaleContext from "./context/LocaleContext";
 
 export default function App() {
-  const [locale, setLocale] = React.useState('cs')
+  const [locale, setLocale] = React.useState("cs");
 
   const toggleLocale = () => {
     setLocale((locale) => {
-      return locale === 'cs' ? 'en' : 'cs'
-    })
-  }
+      return locale === "cs" ? "en" : "cs";
+    });
+  };
 
-  const value = React.useMemo(() => ({
-    locale,
-    toggleLocale
-  }), [locale])
+  const value = React.useMemo(
+    () => ({
+      locale,
+      toggleLocale,
+    }),
+    [locale]
+  );
 
   return (
     <>
       <LocaleContext.Provider value={value}>
         <Router>
-          <Navbar />
+          <Header />
           <Switch>
             <Route path="/" exact />
           </Switch>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,57 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import './Navbar.css'
-import { menuItems } from './MenuItems';
-import flag_cz from './flag_cz.png';
-import flag_en from './flag_en.png';
-import logo from './lg_logo_256.png';
-import LocaleContext from "../../context/LocaleContext"
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { menuItems } from "./MenuItems";
+import flag_cz from "./flag_cz.png";
+import flag_en from "./flag_en.png";
+import logo from "./lg_logo_256.png";
+import LocaleContext from "../../context/LocaleContext";
 
 export default function Navbar() {
-    const { locale, toggleLocale } = React.useContext(LocaleContext)
-    const [ toggleMenu, setToogleMenu ] = React.useState(false);
+  const { locale, toggleLocale } = React.useContext(LocaleContext);
+  const [toggleMenu, setToogleMenu] = React.useState(false);
 
-    const handleToggleMenu = () => setToogleMenu(!toggleMenu);
-    const handleCloseMobileMenu = () => setToogleMenu(false);
-    
-    return (
-        <>
-        <div className="navbar">
-            <Link to="/" className="navbar-logo">
-                <img src={logo} width="70" alt="logo" />
-            </Link>
-            <div className="menu-toggle" onClick={handleToggleMenu}>
-                <i className={`fas fa-${toggleMenu ? "times" : "bars"} fa-2x`}></i>
-            </div>
+  const handleToggleMenu = () => setToogleMenu(!toggleMenu);
+  const handleCloseMobileMenu = () => setToogleMenu(false);
 
-            <nav>
-                <ul className={`nav-menu ${toggleMenu ? "active" : ""}`}>
-                    {menuItems[locale].map((item, index) => {
-                        return (
-                            <li key={index} className="menu-item"><Link to={item.url} activeClassName="active" className={item.cName} onClick={handleCloseMobileMenu}>{item.title}</Link></li>
-                        );
-                    })}
-                    <li className="menu-item">
-                        <Link to="#" className="nav-links-flag" onClick={toggleLocale}>
-                            {locale === 'cs'
-                                ? <img src={flag_en} width="30" alt="en" />
-                                : <img src={flag_cz} width="30" alt="cze" />
-                            }
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className="clearfix"></div>
+  return (
+    <>
+      <div className="navbar">
+        <Link to="/" className="navbar-logo" onClick={handleCloseMobileMenu}>
+          <img src={logo} width="70" alt="logo" />
+        </Link>
+        <div className="menu-toggle" onClick={handleToggleMenu}>
+          <i className={`fas fa-${toggleMenu ? "times" : "bars"} fa-2x`}></i>
         </div>
-        </>
-    );
-}
 
+        <nav>
+          <ul className={`nav-menu ${toggleMenu ? "active" : ""}`}>
+            {menuItems[locale].map((item, index) => {
+              return (
+                <li key={index} className="menu-item">
+                  <Link
+                    to={item.url}
+                    className={item.cName}
+                    onClick={handleCloseMobileMenu}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            })}
+            <li className="menu-item">
+              <Link to="#" className="nav-links-flag" onClick={toggleLocale}>
+                {locale === "cs" ? (
+                  <img src={flag_en} width="30" alt="en" />
+                ) : (
+                  <img src={flag_cz} width="30" alt="cze" />
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="clearfix"></div>
+      </div>
+    </>
+  );
+}
